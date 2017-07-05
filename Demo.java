dcbe82962fc7d2c99dde8d695113051f9b8ddec3
ssoegriemsingh <ssoegriemsingh@qualogy.com>
2017-07-05 15:25:22 +0200

    Added Hit or Stand option.
    - Player hand is dealt
    - Waiting for player input
    - Hit adds a random number
    - Stand exits the loop

--- Demo.java
+++ Demo.java
@@ -1,11 +1,15 @@
 // Imports:
-import java.util.Random;
+import java.util.*;
 
 // Start of class
 public class Demo
 {
 	// Global vars:
 	static String playername;
+	int playerHand = 0;
+	
+	Random r = new Random();
+	Scanner scanner = new Scanner(System.in);
 	
 	// Main:
 	public static void main(String[] args)
@@ -22,16 +26,24 @@ public class Demo
 		Startgame();
 	}
 	
+	// Function to display a greeting:
 	public void Greeting()
 	{
 		String greeting = playername + ", lets play Black Jack!";
 		System.out.println(greeting);
 	}
 	
+	// Function to start the game:
 	public void Startgame()
 	{
-		Random r = new Random();
-		
+		DealDealer();
+		DealPlayer();
+		HitOrStand();
+	}
+	
+	// Function to deal dealerhand and show:
+	public void DealDealer()
+	{
 		int dealerHand = r.nextInt(13) + 1;
 		int hit = r.nextInt(13);
 		
@@ -50,5 +62,36 @@ public class Demo
 		
 		System.out.println("Dealer is showing: " + showing);
 		//System.out.println("Dealer has: " + dealerHand);
+	}
+	
+	// Function to deal playerhand:
+	public void DealPlayer()
+	{
+		playerHand += r.nextInt(13) + 1;
+		System.out.println(playername + " has: " + playerHand);
+	}
+	
+	// Function to hit or stand:
+	public void HitOrStand()
+	{
+		System.out.println("Hit (h) or Stand (s)?");
+		String input = scanner.next();
+		
+		//System.out.println(input);
+		
+		if(input.equals("h"))
+		{
+			DealPlayer();
+			HitOrStand();
+		}
+		else if(input.equals("s"))
+		{
+			//Stand();
+		}
+		else
+		{
+			System.out.println("Please enter a valid command.");
+			HitOrStand();
+		}
 	}
 }
